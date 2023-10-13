cb064dfdb67779b88dd6aa97ad743aae6a3700d4
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-10-13 18:53:32 +0530

    [MH3-86179] tcpip_client_server application regeneration - sam_a7g5_ek G0_freertos

--- apps/tcpip_client_server/firmware/src/config/sam_a7g5_ek_GMAC0_freertos/configuration.h
+++ apps/tcpip_client_server/firmware/src/config/sam_a7g5_ek_GMAC0_freertos/configuration.h
@@ -96,7 +96,7 @@ extern "C" {
 
 #define SYS_CMD_ENABLE
 #define SYS_CMD_DEVICE_MAX_INSTANCES       SYS_CONSOLE_DEVICE_MAX_INSTANCES
-#define SYS_CMD_PRINT_BUFFER_SIZE          2560
+#define SYS_CMD_PRINT_BUFFER_SIZE          2560U
 #define SYS_CMD_BUFFER_DMA_READY
 
 /* Command System Service RTOS Configurations*/
@@ -110,10 +110,10 @@ extern "C" {
 #define SYS_DEBUG_USE_CONSOLE
 
 
-#define SYS_CONSOLE_DEVICE_MAX_INSTANCES   			1
-#define SYS_CONSOLE_UART_MAX_INSTANCES 	   			1
-#define SYS_CONSOLE_USB_CDC_MAX_INSTANCES 	   		0
-#define SYS_CONSOLE_PRINT_BUFFER_SIZE        		200
+#define SYS_CONSOLE_DEVICE_MAX_INSTANCES   			(1U)
+#define SYS_CONSOLE_UART_MAX_INSTANCES 	   			(1U)
+#define SYS_CONSOLE_USB_CDC_MAX_INSTANCES 	   		(0U)
+#define SYS_CONSOLE_PRINT_BUFFER_SIZE        		(200U)
 
 
 
@@ -305,6 +305,7 @@ extern "C" {
 
 #define TCPIP_STACK_TICK_RATE		        		5
 #define TCPIP_STACK_SECURE_PORT_ENTRIES             10
+#define TCPIP_STACK_LINK_RATE		        		333
 
 #define TCPIP_STACK_ALIAS_INTERFACE_SUPPORT   false
 
@@ -412,6 +413,7 @@ extern "C" {
 
 /*** GMAC0 Configuration ***/
 #define DRV_GMAC0
+#define DRV_SAMA7G
 #define TCPIP_GMAC0_TX_DESCRIPTORS_COUNT_DUMMY    1
 #define TCPIP_GMAC0_RX_DESCRIPTORS_COUNT_DUMMY    1
 #define TCPIP_GMAC0_RX_BUFF_SIZE_DUMMY            64
