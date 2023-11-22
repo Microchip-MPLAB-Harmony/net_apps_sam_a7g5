ebd03888c5931835e9b6786d1d5d3e26907f3a37
hrisikesh sahu <hrisikesh.sahu@microchip.com>
2023-11-22 18:08:34 +0530

    [MH3-87196] tcp client applications are updated for sam_a7g5

--- apps/tcpip_tcp_client/firmware/src/app.c
+++ apps/tcpip_tcp_client/firmware/src/app.c
@@ -114,6 +114,7 @@ void APP_Tasks ( void )
     IPV4_ADDR           ipAddr;
     TCPIP_NET_HANDLE    netH;
     int                 i, nNets;
+    static bool         dataReceived=false;
 
     /* Check the application's current state. */
     switch ( appData.state )
@@ -283,7 +284,7 @@ void APP_Tasks ( void )
                     "Host: %s\r\n"
                     "Connection: close\r\n\r\n", appData.path ? appData.path : "null" , appData.host);
             TCPIP_TCP_ArrayPut(appData.socket, (uint8_t*)buffer, strlen(buffer));
-            appData.state = APP_TCPIP_WAIT_FOR_RESPONSE;
+            appData.state = APP_TCPIP_WAIT_FOR_RESPONSE;            
         }
         break;
 
@@ -295,6 +296,12 @@ void APP_Tasks ( void )
             {
                 TCPIP_TCP_ArrayGet(appData.socket, (uint8_t*)buffer, sizeof(buffer) - 1);
                 SYS_CONSOLE_PRINT("%s", buffer);
+                dataReceived = true;
+            }
+            if(dataReceived)
+            {
+                SYS_CONSOLE_PRINT("\r\nConnection established with < %s > \r\n",appData.host);
+                dataReceived = false;
             }
 
             if (!TCPIP_TCP_IsConnected(appData.socket) || TCPIP_TCP_WasDisconnected(appData.socket))
